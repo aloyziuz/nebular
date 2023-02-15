58a4d3fe95b2be4e6d481cfe153126d5927ded55
Aloysius <aloyziuz@users.noreply.github.com>
2023-02-15 09:51:05 +0700

    replaced stepper animation with angular animation

--- src/framework/theme/components/stepper/stepper.component.ts
+++ src/framework/theme/components/stepper/stepper.component.ts
@@ -20,6 +20,7 @@ import { convertToBoolProperty, NbBooleanInput } from '../helpers';
 import { NB_STEPPER } from './stepper-tokens';
 import { NbStepComponent } from './step.component';
 import { Subscription } from 'rxjs';
+import { animate, sequence, style, transition, trigger } from '@angular/animations';
 
 export type NbStepperOrientation = 'vertical' | 'horizontal';
 
@@ -125,6 +126,18 @@ export interface NbStepChangeEvent {
   styleUrls: ['./stepper.component.scss'],
   templateUrl: './stepper.component.html',
   providers: [{ provide: NB_STEPPER, useExisting: NbStepperComponent }],
+  animations: [
+    trigger('transitionAnim', [
+      transition(
+        '* => *',
+        sequence([
+          style({ opacity: 1, transform: 'scale(1)' }),
+          animate('100ms cubic-bezier(0.785, 0.135, 0.15, 0.86)', style({ opacity: 0, transform: 'scale(0.2)' })),
+          animate('200ms cubic-bezier(0.785, 0.135, 0.15, 0.86)', style({ opacity: 1, transform: 'scale(1)' })),
+        ]),
+      ),
+    ]),
+  ],
 })
 export class NbStepperComponent implements OnInit, OnDestroy {
   /**
